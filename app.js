98792ef36c45ddea7eb8388a0c23b26eb9bc4ac8
jv699 <jvacarch@gmail.com>
2023-07-27 20:58:33 -0400

    mid lesson push

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = Vue.createApp({
             author: 'Brandon Sanderson',
             age: 45
         };
+    },
+    methods: {
+        changeTitle(title) {
+            this.title = title;
+        }
     }
 });
 
