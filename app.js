e0255a67ad822a1709e1ca7fbb4659496469d84e
jv699 <jvacarch@gmail.com>
2023-07-27 20:47:16 -0400

    mid lesson push

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    //this object is the 'root component'. data and function that react
+    // template: '<h2>I am a template</h2>' // this will be injected into the browser               
+    data() { //this is shorthand for a function in an object
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        };
+    }
+});
+
+app.mount('#app');
