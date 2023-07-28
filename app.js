663a2918019a78f42686d11754a355d028982cc9
jv699 <jvacarch@gmail.com>
2023-07-27 21:09:18 -0400

    lesson one

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const app = Vue.createApp({
     //this object is the 'root component'. data and function that react
     // template: '<h2>I am a template</h2>' // this will be injected into the browser               
-    data() { //this is shorthand for a function in an object
+    data() { //this is shorthand for a function in an object. these are the variables that are accessible
         return {
+            bShowBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
         };
     },
     methods: {
-        changeTitle(title) {
-            this.title = title;
+        // changeTitle(title) {
+        //     this.title = title;
+        // }
+        toggleShowBooks() {
+            this.bShowBooks = !this.bShowBooks; 
         }
     }
 });
